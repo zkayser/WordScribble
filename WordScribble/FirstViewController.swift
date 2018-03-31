9b8f9d91d228b8cab1351689aeaf068111fd8c90
Zack.Kayser <zack.kayser@kroger.com>
2018-03-31 15:23:24 -0400

    Initial Commit

---
+++ WordScribble/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  WordScribble
+//
+//  Created by Kayser, Zack (NonEmp) on 3/31/18.
+//  Copyright © 2018 Kayser, Zack (NonEmp). All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ WordScribble/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  WordScribble
+//
+//  Created by Kayser, Zack (NonEmp) on 3/31/18.
+//  Copyright © 2018 Kayser, Zack (NonEmp). All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
